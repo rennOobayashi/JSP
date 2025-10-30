d244621f5edc213c1d032615036e1944b2fe2f25
inhee <devforplayer@gmail.com>
2025-10-30 15:48:09 +0900

    v7.3 homework MVC indexing

--- Week9_04Introduce/src/main/java/com/example/demo/MyController.java
+++ Week9_04Introduce/src/main/java/com/example/demo/MyController.java
@@ -5,22 +5,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MyController {	
+public class MyController {
+	private int index = -1;
+	private String str = "";
+	
 	@RequestMapping("/")
 	@ResponseBody
 	public String root() {
-		return "안녕하세요, 이인희입니다.";
+		if (index < 1) {
+		str += "안녕하세요, 이인희입니다.<br>";
+		}
+		else {
+			str = "안녕하세요, 이인희입니다.<br>";
+		}
+		
+		index = 1;
+		
+		return str;
 	}
 	
 	@RequestMapping("/intro")
 	@ResponseBody
 	public String intro() {
-		return "나는 21살이고, MBTI는 모릅니다.";
+		if (index < 2) {
+		str += "나는 21살이고, MBTI는 모릅니다.<br>";
+		}
+		else {
+			str = "나는 21살이고, MBTI는 모릅니다.<br>";
+		}
+		
+		index = 2;
+		
+		return str;
 	}
 	
 	@RequestMapping("/sports")
 	@ResponseBody
 	public String sports() {
-		return "좋아하는 운동은 달리기랑 농구입니다.";
+		if (index < 3) {
+		str += "좋아하는 운동은 달리기랑 농구입니다.<br>";
+		}
+		else {
+			str = "좋아하는 운동은 달리기랑 농구입니다.<br>";
+		}
+		
+		index = 3;
+		
+		return str;
 	}
 }
