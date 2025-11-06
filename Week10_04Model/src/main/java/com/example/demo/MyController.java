3a6834a449f09ab178d3567cbc124ed15543e590
inhee <devforplayer@gmail.com>
2025-11-06 15:58:37 +0900

    v8.4 Model (not work)

---
+++ Week10_04Model/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {	
+	@RequestMapping("/")
+	@ResponseBody
+	public String root() {
+		return "Model and view";
+	}
+	
+	@RequestMapping("/model")
+	public String modelMethod(Model model) {
+		//give data to view using model
+		//Only save data
+		model.addAttribute("name", "Lee Inhee");
+		model.addAttribute("age", "20");
+		
+		return "model";
+	}
+	
+	//Model and View
+	@GetMapping("/mv")
+	public ModelAndView mvMethod() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", "Lee Inhee");
+		mv.addObject("age", "20");
+		
+		List<String> ls = new ArrayList();
+		ls.add("test1");
+		ls.add("test2");
+		ls.add("test3");
+		
+		mv.addObject("lists", ls);
+		
+		mv.setViewName("myView");
+		
+		return mv;
+	}
+}
